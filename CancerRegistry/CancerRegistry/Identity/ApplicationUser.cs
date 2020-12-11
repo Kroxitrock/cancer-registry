3a7cfe5c396881603df8241e48224b2a3b52a7b7
LyubomirF <lubo99filipov@gmail.com>
2020-12-12 00:46:16 +0200

    Added application user class. #10

---
+++ CancerRegistry/CancerRegistry/Identity/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancerRegistry.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+
+        public String EGN { get; set; }
+
+        public String UID { get; set; }
+    }
+}
