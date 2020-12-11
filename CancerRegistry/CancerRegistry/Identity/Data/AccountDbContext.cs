8c9d0bfef3fdb5645ff4f40f0a4ad82af5947c58
LyubomirF <lubo99filipov@gmail.com>
2020-12-12 00:46:57 +0200

    Added account database context class. #10

---
+++ CancerRegistry/CancerRegistry/Identity/Data/AccountDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancerRegistry.Identity.Data
+{
+    public class AccountDbContext : IdentityDbContext
+    {
+        public AccountDbContext(DbContextOptions<AccountDbContext> options)
+            : base(options) { }
+    }
+}
