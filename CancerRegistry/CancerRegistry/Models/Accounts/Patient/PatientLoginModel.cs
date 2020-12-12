d384e6a2e7e8346f69aac23b1714b1e407248f0a
LyubomirF <lubo99filipov@gmail.com>
2020-12-12 14:12:50 +0200

    Created login model for patient.

---
+++ CancerRegistry/CancerRegistry/Models/Accounts/Patient/PatientLoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancerRegistry.Models.Accounts.Patient
+{
+    public class PatientLoginModel
+    {
+        public string EGN { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
