d0025ffe68354d40f1b4808026ecc117b1d342fb
LyubomirF <lubo99filipov@gmail.com>
2020-12-12 14:13:17 +0200

    Created register model for patient.

---
+++ CancerRegistry/CancerRegistry/Models/Accounts/Patient/PatientRegisterModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancerRegistry.Models.Accounts.Patient
+{
+    public class PatientRegisterModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string EGN { get; set; }
+
+        [Required, DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password), Compare(nameof(Password))]
+        public string RepeatPassoword { get; set; }
+    }
+}
