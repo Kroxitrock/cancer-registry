9d619fddc4c9dd99ef22e2cfaa8bce1f04c6a220
LyubomirF <lubo99filipov@gmail.com>
2020-12-12 16:06:27 +0200

    Implemented register patient method. #10

--- CancerRegistry/CancerRegistry/Services/AccountService.cs
+++ CancerRegistry/CancerRegistry/Services/AccountService.cs
@@ -12,8 +12,6 @@ namespace CancerRegistry.Services
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public List<string> LoginErrors { get; set; }
-
         public AccountService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
@@ -33,7 +31,32 @@ namespace CancerRegistry.Services
             return signInResult.Succeeded;
         }
 
-        public async Task Logoutuser()
+        public async Task LogoutUser()
          => await _signInManager.SignOutAsync();
+
+        public async Task<bool> RegisterPatient(
+            string firstName,
+            string lastName,
+            string egn,
+            string phoneNumber,
+            string password)
+        {
+            ApplicationUser user = new ApplicationUser()
+            {
+                UserName = egn,
+                FirstName = firstName,
+                LastName = lastName,
+                EGN = egn,
+                PhoneNumber = phoneNumber
+            };
+
+            var registerResult = await _userManager.CreateAsync(user, password);
+            var roleResult = await _userManager.AddToRoleAsync(user,"Patient");
+
+            if (!registerResult.Succeeded || !roleResult.Succeeded)
+                return false;
+
+            return true;
+        }
     }
 }
