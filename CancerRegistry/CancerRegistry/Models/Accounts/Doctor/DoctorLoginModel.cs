d4d66499e79494a63e8b6ddf5c5758ca6c70a169
LyubomirF <lubo99filipov@gmail.com>
2020-12-12 14:12:13 +0200

    Created login model for doctor.

---
+++ CancerRegistry/CancerRegistry/Models/Accounts/Doctor/DoctorLoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancerRegistry.Models.Accounts.Doctor
+{
+    public class DoctorLoginModel
+    {
+        public string UID { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
