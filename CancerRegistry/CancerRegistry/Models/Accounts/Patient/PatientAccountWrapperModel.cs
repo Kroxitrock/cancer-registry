d4e5da7ed283cbbc2b8171d95e7ec2acb3fa57cb
LyubomirF <lubo99filipov@gmail.com>
2020-12-12 17:37:08 +0200

    Added wrapper model class for patient's login and register information.

---
+++ CancerRegistry/CancerRegistry/Models/Accounts/Patient/PatientAccountWrapperModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancerRegistry.Models.Accounts.Patient
+{
+    public class PatientAccountWrapperModel
+    {
+        public PatientLoginModel LoginModel { get; set; }
+        public PatientRegisterModel RegisterModel { get; set; }
+    }
+}
