1b1d7e23ffb5c0caa518d41a419b4ee76fdb0cba
LyubomirF <lubo99filipov@gmail.com>
2021-01-15 18:51:56 +0200

    Added Bulstat property.

--- CancerRegistry/CancerRegistry/Migrations/20210115100708_AddedBustat.Designer.cs
+++ CancerRegistry/CancerRegistry/Migrations/20210115100708_AddedBustat.Designer.cs
@@ -4,14 +4,16 @@ using CancerRegistry.Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CancerRegistry.Migrations
 {
     [DbContext(typeof(AccountDbContext))]
-    partial class AccountDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210115100708_AddedBustat")]
+    partial class AddedBustat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CancerRegistry/CancerRegistry/Migrations/20210115100708_AddedBustat.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CancerRegistry.Migrations
+{
+    public partial class AddedBustat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HospitalBulstat",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HospitalBulstat",
+                table: "AspNetUsers");
+        }
+    }
+}
