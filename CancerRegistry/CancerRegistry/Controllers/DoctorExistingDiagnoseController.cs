4cf53e9e3f119b073da7b665c4e27b55f8ace2f8
Andrey Strinski <andrey.strinski@musala.com>
2021-01-16 12:06:08 +0200

    [add] Initial Diagnose views

---
+++ CancerRegistry/CancerRegistry/Controllers/DoctorExistingDiagnoseController.cs
@@ -0,0 +1,29 @@
+﻿using CancerRegistry.Models.Diagnoses;
+using CancerRegistry.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancerRegistry.Controllers
+{
+    public class DoctorExistingDiagnoseController : Controller
+    {
+        private readonly DiagnoseService _diagnoseService;
+
+        public DoctorExistingDiagnoseController(DiagnoseService diagnoseService)
+        {
+            _diagnoseService = diagnoseService;
+        }
+
+        [Authorize(Roles = "Doctor")]
+        public IActionResult Index(long diagnoseId)
+        {
+            Diagnose diagnose = _diagnoseService.GetById(diagnoseId);
+
+            return View("/Views/Diagnose/DoctorExistingDiagnoseView.cshtml");
+        }
+    }
+}
